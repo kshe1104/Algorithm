7bf143768ef636289787fc996e505d811b61807c
Kim Tae Jin <149847128+kshe1104@users.noreply.github.com>
2025-12-11 16:16:07 +0900

    [level 2] Title: 전화번호 목록, Time: 315.81 ms, Memory: 252 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public boolean solution(String[] phone_book) {Set<String> set = new HashSet<>();
+                                                  Arrays.stream(phone_book).forEach(set::add);
+                                                  
+                                                  for(String phone: phone_book){
+                                                      for(int i=1;i<phone.length();i++){
+                                                          if(set.contains(phone.substring(0,i)))
+                                                              return false;
+                                                      }
+                                                  }
+        return true;
+    }
+} 
