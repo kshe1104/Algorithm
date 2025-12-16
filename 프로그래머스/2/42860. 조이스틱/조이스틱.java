96ac4fec856d882de1bd1facd38f128c713bad0b
Kim Tae Jin <149847128+kshe1104@users.noreply.github.com>
2025-12-16 16:56:48 +0900

    [level 2] Title: 조이스틱, Time: 0.06 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,26 +1,26 @@
 import java.util.*;
 class Solution {
     public int solution(String name) {
-                int len = name.length();
-        int result = 0;
-        char[] joy = new char[len];
-
+        int answer = 0;
+        int len = name.length();
         for(int i=0;i<len;i++){
-char c = name.charAt(i);
-            result+=Math.min(c-'A','Z'-c+1);// 위 아래 최소조작
+            char c = name.charAt(i);
+            answer+=Math.min(c-'A','Z'-c+1);
         }
-        // 좌/우 최소이동
-        int move = len-1;//일단 끝까지 쭉 가는 경우
+        // 좌/우 최소 이동
+        int move = len-1; // 일단 끝까지 가는 경우 -> 움직임이 len-1만큼 일어남
         for(int i=0;i<len;i++){
-            int next = i+1;
-            // 연속된 'A'구간 찾기
-            while(next<len && name.charAt(next)=='A'){next++;}
-            //오른족으로 갔다가 되돌아오는 경우들을 모두 고려
-            int case1 = 2*i+len-next;
-            int case2 = i+2*(len-next);
+            int next = i+1; 
+           
+        while(next<len && name.charAt(next)=='A'){
+            next++; // A가 겹쳐있는 구간 건너뛰기
+        }
+            // 오른쪽으로 갔다가 되돌아오는 경우들을 모두 고려
+            int case1 = 2*i+len-next; //오른쪽 갔다가 왼쪽
+            int case2 = i+2*(len-next); //왼쪽(끝) 쪽으로 크게 돌아오기
             move = Math.min(move,Math.min(case1,case2));
         }
-        result+=move;
-        return result;
+        answer+=move;
+        return answer;
     }
 }
