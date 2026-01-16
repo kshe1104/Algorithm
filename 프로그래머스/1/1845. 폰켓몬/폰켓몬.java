5a971b28212400debaaeceffd7323fc7f03b2acc
Kim Tae Jin <149847128+kshe1104@users.noreply.github.com>
2026-01-16 15:17:38 +0900

    [level 1] Title: 폰켓몬, Time: 5.15 ms, Memory: 92.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,14 +1,15 @@
 import java.util.*;
 class Solution {
     public int solution(int[] nums) {
-        HashSet h = new HashSet();
+        HashSet h = new HashSet(); // HashSet 생성 크기 정적으로 잡을 필요 없음
+        
         for(int i=0;i<nums.length;i++){
-            h.add(nums[i]);
+            h.add(nums[i]); //전부 다 hashSet에 넣음(중복 허용하지않기때문에)
         }
         
-        if (h.size()<nums.length/2){
-            return h.size();
-        }else{
+        if (h.size() < nums.length/2){
+            return h.size(); //만약 HashSet의 사이즈가 포켓몬의 수의 절반보다 작다면 사이즈 리턴
+        } else{
             return nums.length/2;
         }
     }
