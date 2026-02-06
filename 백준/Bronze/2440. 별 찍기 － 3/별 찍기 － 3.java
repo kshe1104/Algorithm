80b3e1d11bf9e60a6784edbc411d0bbbee94cc00
Kim Tae Jin <149847128+kshe1104@users.noreply.github.com>
2026-02-06 14:19:29 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 240 ms, Memory: 18124 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sr = new Scanner(System.in);
+        int n = sr.nextInt();
+        for(int i=n;i>0;i--){
+            for(int j=0;j<i;j++){
+            System.out.print("*");}
+            System.out.println("");
+        }
+    }
+}
