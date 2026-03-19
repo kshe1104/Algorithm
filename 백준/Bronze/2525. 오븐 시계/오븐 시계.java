9f14a9f15605c8a1ba8509f7736a541a10eee017
Kim Tae Jin <149847128+kshe1104@users.noreply.github.com>
2026-03-19 12:49:03 +0900

    [Bronze III] Title: 오븐 시계, Time: 196 ms, Memory: 18464 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sr = new Scanner(System.in);
+        int hour = sr.nextInt();
+        int minute = sr.nextInt();
+        int cook = sr.nextInt();
+        int cook_hr = cook / 60;
+        int cook_min = cook % 60;
+
+        hour += cook_hr;
+        minute += cook_min;
+
+        if (minute > 59) {
+            hour++;
+            minute = minute - 60;
+        }
+
+        if(hour>23){
+            hour%=24;
+        }
+        System.out.println(hour + " " + minute);
+    }
+}
