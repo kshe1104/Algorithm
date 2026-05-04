763164cefd6611d32d9c2e213f40c71d561c3571
Kim Tae Jin <149847128+kshe1104@users.noreply.github.com>
2026-05-04 13:49:43 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.38 ms, Memory: 55.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 55 MB, 시간: 8.83 ms
+메모리: 55.3 MB, 시간: 9.38 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 14일 15:54:53
+2026년 05월 04일 13:49:37
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,46 +1,50 @@
 import java.util.*;
 class Solution {
-       int[] dx = {0, 1, -1, 0};
-    int[] dy = {1, 0, 0, -1};
     public int solution(int[][] maps) {
-        int answer = 0;
-        int[][] visited = new int[maps.length][maps[0].length];
-        bfs(maps,visited);
-        answer = visited[maps.length-1][maps[0].length-1];
-        if(answer==0){
-            answer = -1;
-        }
-        return answer;
-    }
-    
-        public void bfs(int[][] maps, int[][] visited) {
+        int n = maps.length; // 행
+        int m = maps[0].length; // 열
+        
+        // BFS를 위한 큐 준비
         Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{0, 0}); //큐에 시작정점 추가
-
-        visited[0][0] = 1;
-
-        while(!queue.isEmpty())
-        {
-            int[] current = queue.poll(); //정점 하나 꺼내기
-            int X = current[0];
-            int Y = current[1];
-
-            for (int i = 0; i < 4; i++) {
-                int nX = X + dx[i];
-                int nY = Y + dy[i];
-
-                //좌표가 maps에서 벗어날 경우 다음 반복 실행
-                if(nX <0||nX>maps.length-1||nY<0||nY>maps[0].length-1){
-                    continue;
-                }
-
-                //아직 방문하지 않았고, 벽이 아닐경우
-                if(visited[nX][nY]==0&&maps[nX][nY]==1){
-                    visited[nX][nY] = visited[X][Y] + 1;
-                    queue.add(new int[]{nX, nY});
+        
+        // 방향 배열
+        int[] dr = {1,-1,0,0}; // 상,하
+        int[] dc = {0,0,1,-1}; // 좌,우
+        
+        boolean[][] visited = new boolean[n][m]; // maps의 크기만큼 방문배열
+        
+        // 초기화
+        // 시작 위치(0,0)와 현재까지의 거리(1)을 큐에 넣기
+        queue.add(new int[]{0,0,1}); // 크기 3짜리 배열을 큐에 삽입
+        visited[0][0] = true; // 시작 위치 방문 처리
+        
+        // 반복
+        while(!queue.isEmpty()){
+            int [] current = queue.poll(); // 큐에서 하나 꺼냄
+            int r = current[0];
+            int c = current[1];
+            int dist = current[2];
+            
+            // 목적지(상대 팀 진영)에 도착했다면?
+            if(r==n-1&&c==m-1){
+                return dist;
+            }
+            
+            // 주변 4방향 탐색
+            for(int i=0;i<4;i++){
+                int nr = r+dr[i]; // 현재 위치에서 상,하
+                int nc = c+dc[i]; // 현재 위치에서 좌,우
+                
+                // 맵 안이고, 벽이 아니고, 아직 안가본 곳이라면?
+                if(nr>=0&nr<n&&nc>=0&&nc<m){
+                    if(maps[nr][nc]==1&&!visited[nr][nc]){ // 아직 안가본 곳이라면
+                        visited[nr][nc] = true; // 방문표시
+                        queue.add(new int[]{nr,nc,dist+1}); // 큐에 넣기(거리+1);
+                    }
                 }
             }
         }
+        // 끝까지 못갔다면 못가는 막혀있는 곳이므로 -1 반환
+        return -1;
     }
-
 }
