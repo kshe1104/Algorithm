b7ed7bf50b822a6bb9f2056aeaf87b0391f9e7b6
Kim Tae Jin <149847128+kshe1104@users.noreply.github.com>
2026-05-26 15:37:57 +0900

    [level 2] Title: 가장 큰 수, Time: 96.55 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,22 +1,26 @@
 import java.util.*;
-class Solution {
-    public String solution(int[] numbers) {
-        
-        String[] str = new String[numbers.length];
-        for (int i = 0; i < numbers.length; i++) {
-            str[i] = String.valueOf(numbers[i]); //String으로
-        }
-        
-        Arrays.sort(str,(o1,o2)->(o2+o1).compareTo(o1+o2));
-        
- if(str[0].equals("0")){
-            return "0";
-        } // 정렬했는데 0이 가장 크다면 0 리턴
-        
+class Solution{
+        public String solution(int[] numbers){
+            String[] strNumbers = new String[numbers.length]; // numbers를 String으로 바꿔줄 배열 생성
+            for (int i = 0; i < numbers.length; i++) {
+                strNumbers[i] = String.valueOf(numbers[i]); // int[] => String[]
+            }
+            
+            
+            // 2. 사전 순x, 이어 붙였을 때 큰 순서로 내림차순 설정
+            Arrays.sort(strNumbers, new Comparator<String>() {
+                @Override
+                public int compare(String o1, String o2) {
+                    return (o2 + o1).compareTo(o1 + o2); // o2+o1과 o1+o2를 비교해서 큰 것을 앞으로 오게함(내림차순)
+                }
+            });
+            
+            if(strNumbers[0].equals("0")) return "0";
+            // 정렬 결과 예시: [34,3,30]
             StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < str.length; i++) {
-            sb.append(str[i]); // 차례대로 더해줌
+            for (String str : strNumbers) {
+                sb.append(str);
+            }
+            return sb.toString();
         }
-        return sb.toString();
     }
-}
