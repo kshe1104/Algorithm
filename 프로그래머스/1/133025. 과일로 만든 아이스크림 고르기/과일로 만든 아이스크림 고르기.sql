40098218d5541be1d3cea9f7b9f838a31ed743e7
Kim Tae Jin <149847128+kshe1104@users.noreply.github.com>
2026-09-09 12:34:57 +0900

    [level 1] Title: 과일로 만든 아이스크림 고르기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/133025. 과일로 만든 아이스크림 고르기/과일로 만든 아이스크림 고르기.sql
+++ 프로그래머스/1/133025. 과일로 만든 아이스크림 고르기/과일로 만든 아이스크림 고르기.sql
@@ -1,12 +1,5 @@
-# select f.flavor from FIRST_HALF f
-# join Icecream_Info i 
-# on f.flavor = i.flavor
-# where f.total_order>3000 and
-# i.ingredient_type like 'fruit_based'
-# order by total_order desc;
-
-select f.flavor from FIRST_HALF f 
+select f.flavor from first_half f
 join icecream_info i
 on f.flavor = i.flavor
-where f.total_order>3000 and i.ingredient_type like 'fruit_based'
+where f.total_order > 3000 and i.ingredient_type like 'fruit_based'
 order by f.total_order desc;
