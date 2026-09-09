58319c33a7a45ec96b9e79c5721faf75cd69ee41
Kim Tae Jin <149847128+kshe1104@users.noreply.github.com>
2026-09-09 13:14:45 +0900

    [level 1] Title: 흉부외과 또는 일반외과 의사 목록 출력하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/132203. 흉부외과 또는 일반외과 의사 목록 출력하기/흉부외과 또는 일반외과 의사 목록 출력하기.sql
+++ 프로그래머스/1/132203. 흉부외과 또는 일반외과 의사 목록 출력하기/흉부외과 또는 일반외과 의사 목록 출력하기.sql
@@ -7,7 +7,11 @@
 # where mcdp_cd = 'CS' or mcdp_cd='GS'
 # order by hire_YMD desc, dr_name asc;
 # -- 내림차순 desc , 오름차순 asc
-select dr_name, dr_id, mcdp_cd, date_format(hire_ymd,"%Y-%m-%d") as hire_ymd
-from doctor
-where mcdp_cd like 'CS' or mcdp_cd = 'GS'
+# select dr_name, dr_id, mcdp_cd, date_format(hire_ymd,"%Y-%m-%d") as hire_ymd
+# from doctor
+# where mcdp_cd like 'CS' or mcdp_cd = 'GS'
+# order by hire_ymd desc, dr_name asc;
+
+select dr_name,dr_id,mcdp_cd,hire_ymd from doctor
+where mcdp_cd like 'GS' || mcdp_cd like 'CS'
 order by hire_ymd desc, dr_name asc;
