4f9cfd9c3b383119d4c4762c3956f93bcc6293c2
Kim Tae Jin <149847128+kshe1104@users.noreply.github.com>
2026-09-10 12:01:01 +0900

    [level 1] Title: 강원도에 위치한 생산공장 목록 출력하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/131112. 강원도에 위치한 생산공장 목록 출력하기/강원도에 위치한 생산공장 목록 출력하기.sql
+++ 프로그래머스/1/131112. 강원도에 위치한 생산공장 목록 출력하기/강원도에 위치한 생산공장 목록 출력하기.sql
@@ -3,3 +3,7 @@ SELECT factory_id , factory_name, address
 from food_factory
 where address like '강원도%'
 order by factory_id asc;
+
+select factory_id,factory_name,address from food_factory
+where address like '강원도%'
+order by factory_id asc;
