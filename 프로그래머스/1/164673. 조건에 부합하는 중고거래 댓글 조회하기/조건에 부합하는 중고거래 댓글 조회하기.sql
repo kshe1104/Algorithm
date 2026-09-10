a9844e051947d3dfc3b5c92bd6cf76c5b0d0384d
Kim Tae Jin <149847128+kshe1104@users.noreply.github.com>
2026-09-10 11:59:17 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,13 +1,23 @@
--- 코드를 입력하세요
-SELECT 
-B.Title, 
-B.Board_ID, 
-R.reply_id,
-R.Writer_id,
-R.Contents,
-DATE_FORMAT(R.created_Date,'%Y-%m-%d') as CREATED_DATE
-from used_goods_board B
-JOIN used_goods_reply R
-    on B.board_id = R.board_id
-where B.Created_Date like '2022-10%'
-order by Created_DATE asc , title asc;
+# -- 코드를 입력하세요
+# SELECT 
+# B.Title, 
+# B.Board_ID, 
+# R.reply_id,
+# R.Writer_id,
+# R.Contents,
+# DATE_FORMAT(R.created_Date,'%Y-%m-%d') as CREATED_DATE
+# from used_goods_board B
+# JOIN used_goods_reply R
+#     on B.board_id = R.board_id
+# where B.Created_Date like '2022-10%'
+# order by Created_DATE asc , title asc;
+
+
+select b.title, b.board_id, r.reply_id, r.writer_id, r.contents,r.created_date from used_goods_board b join used_goods_reply r
+on b.board_id = r.board_id
+where b.created_date like '2022-10%'
+order by r.created_date asc, b.title asc;
+
+
+
+
